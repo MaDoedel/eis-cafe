24b5ee069a71b64517a91465921eb1cd9a976760
unknown <ckhuemonma@web.de>
2024-03-30 18:24:30 +0100

    navbar functionality

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,10 +1,13 @@
 window.onload = function() {
-    for (let i = 0; i < document.getElementById("navbar").children.length; i++) {
-        document.getElementById("navbar").children[i].addEventListener("click", function() {
-            for (let j = 0; j < document.getElementById("navbar").children.length; j++) {
-                document.getElementById("navbar").children[j].classList.remove("active");
-            }
-            this.classList.add("active");
+    let selected_index = 0;
+
+    for (let i = 0; i < document.getElementById("navbar").children.length - 1; i++) {
+        document.getElementById("navbar").children[i+1].addEventListener("click", function() {
+            document.getElementById("navbar").children[selected_index+1].classList.remove("selected")
+            document.getElementById("content-"+ selected_index).classList.remove("selected")
+            selected_index = i
+            this.classList.add("selected")
+            document.getElementById("content-"+ i).classList.add("selected")
         });
     }
 }
