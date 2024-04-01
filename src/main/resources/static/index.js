e87113fb12ec45c3855d62b08f852e6843c25505
unknown <ckhuemonma@web.de>
2024-04-01 17:14:17 +0200

    bootstrap integration

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,16 +1,4 @@
 window.onload = function() {
-    let selected_index = 0;
-
-    for (let i = 0; i < document.getElementById("navbar").children.length - 1; i++) {
-        document.getElementById("navbar").children[i+1].addEventListener("click", function() {
-            document.getElementById("navbar").children[selected_index+1].classList.remove("selected")
-            document.getElementById("content-"+ selected_index).classList.remove("selected")
-            selected_index = i
-            this.classList.add("selected")
-            document.getElementById("content-"+ i).classList.add("selected")
-        });
-    }
-
     function refetchIce() {
         $.ajax({
             url: '/ice',
